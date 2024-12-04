7240b02c23666169b0e9ceadb3e14cbdf4df2a7e
Fedor Pchelkin <pchelkin@ispras.ru>
2024-12-04 22:24:43 +0003

    solve task-2

--- 02-func-name-suggestion/funccraft/data.py
+++ 02-func-name-suggestion/funccraft/data.py
@@ -1,11 +1,144 @@
-from pathlib import Path
-
 import datasets
+import pandas as pd
+import tree_sitter_python as tspython
+from pathlib import Path
+from tree_sitter import Language, Parser
+
+
+PY_LANGUAGE = Language(tspython.language())
+parser = Parser(PY_LANGUAGE)
+
+
+def print_example():
+    example = """def sina_xml_to_url_list(xml_data):
+        \"\"\"str->list
+        Convert XML to URL List.
+        From Biligrab.
+        \"\"\"
+        rawurl = []
+        # Comment1
+        # Comment 2
+        dom = parseString(xml_data)
+        for node in dom.getElementsByTagName('durl'):
+            url = node.getElementsByTagName('url')[0]  # Comment 3
+            rawurl.append(url.childNodes[0].data)
+        return rawurl
+    """
+    print("Example")
+    print(example)
+    function_name, body_with_comments, body_without_comments = (
+        extract_function_info(example)
+    )
+    print("Function name: ", function_name)
+    print("Body with comments: \n", body_with_comments)
+    print("Body without comments: \n", body_without_comments)
+
+
+def pretty_print(fail):
+    print_example()
+    print("Number of failed func names:", fail)
+
+
+def delete_symbols(delete_ranges, code):
+    if not delete_ranges:
+        return code
+
+    parts = [code[0 : delete_ranges[0][0]]]
+    for i in range(len(delete_ranges) - 1):
+        parts.append(code[delete_ranges[i][1] : delete_ranges[i + 1][0]])
+    parts.append(code[delete_ranges[-1][1] :])
+
+    return ''.join(parts)
+
+
+def delete_comments(node):
+    comments = []
+
+    for child in node.children:
+        if child.type == 'comment':
+            comments.append((child.start_byte, child.end_byte))
+        elif (
+            child.type == 'expression_statement'
+            and child.children[0].type == 'string'
+            and child.parent.parent.type == 'function_definition'
+        ):
+            comments.append((child.start_byte, child.end_byte))
+
+        comments.extend(delete_comments(child))
+
+    return comments
+
+
+def extract_function_info(code):
+    tree = parser.parse(bytes(code, "utf8"))
+    function_name = None
+    body_with_comments = ""
+    body_without_comments = ""
+
+    for child in tree.root_node.children:
+        if child.type == "function_definition":
+            function_name = child.child_by_field_name("name").text.decode(
+                "utf-8"
+            )
+            body_with_comments = child.text.decode("utf-8")
+            to_delete = delete_comments(child.child_by_field_name("body"))
+
+            body_without_comments = (
+                delete_symbols(to_delete, body_with_comments)
+                if to_delete
+                else body_with_comments
+            )
+
+            body_without_comments = '\n'.join(
+                filter(None, map(str.strip, body_without_comments.split('\n')))
+            )
+
+            body_without_comments, body_with_comments = map(
+                lambda x: x.replace(function_name, "<extra_id_0>", 1),
+                [body_without_comments, body_with_comments],
+            )
+
+    return function_name, body_with_comments, body_without_comments
+
+
+def check_correctness(dataset):
+    n_fail = 0
+
+    for i in range(1100):
+        cur = dataset["func_name"][i].rsplit(".", 1)
+        cur = cur[1] if len(cur) > 1 else cur[0]
+        curr = dataset["custom_fn_name"][i]
+        if cur != curr:
+            n_fail += 1
+
+    pretty_print(n_fail)
 
 
 def prepare() -> datasets.Dataset:
-    # Implement dataset preparation code here
-    return datasets.Dataset.from_dict({'fake_data': [[1, 2], [3, 4]]})
+    dataset = datasets.load_dataset(
+        "code_search_net", "python", split='test', trust_remote_code=True
+    ).select(range(1100))
+    dataset = pd.DataFrame(dataset)
+    data = dataset['whole_func_string']
+    func_names = []
+    with_comments = []
+    without_comments = []
+
+    for i in data:
+        function_name, body_with_comments, body_without_comments = (
+            extract_function_info(i)
+        )
+        func_names.append(function_name)
+        with_comments.append(body_with_comments)
+        without_comments.append(body_without_comments)
+
+    dataset['custom_fn_name'] = func_names
+    dataset['custom_with_comments'] = with_comments
+    dataset['custom_without_comments'] = without_comments
+
+    check_correctness(dataset)
+
+    return datasets.Dataset.from_pandas(dataset)
 
 
 def load_dataset(path: Path) -> datasets.Dataset:
--- 02-func-name-suggestion/funccraft/models.py
+++ 02-func-name-suggestion/funccraft/models.py
@@ -1,9 +1,12 @@
+import datasets
+import evaluate
+import torch
 from collections.abc import Iterable
 from functools import cache
 from pprint import pprint
+from transformers import T5ForConditionalGeneration, AutoTokenizer
 
-import datasets
-import evaluate
+device = torch.device("cpu")
 
 
 @cache
@@ -11,18 +14,49 @@ def _init_metrics():
     return (evaluate.load('exact_match'), evaluate.load('rouge'))
 
 
-def predict(dataset: datasets.Dataset, model: str) -> None:
-    # Implement your function name prediction loop here
-    predictions = ['func_one', 'func_three']
-    references = ['func_one', 'func_two']
+def pretty_print(title, data):
+    print()
+    print('*' * 80)
+    print(title)
+    pprint(data)
+    print('*' * 80)
+    print()
 
-    eval_results = run_evaluate(predictions=predictions, references=references)
-    print()
-    print('*' * 80)
-    print('Evaluation results:')
-    pprint(eval_results)
-    print('*' * 80)
-    print()
+
+def predict_function_name(tokenizer, model, function_body):
+    inputs = tokenizer.encode(function_body, return_tensors="pt").to(device)
+    outputs = model.generate(inputs, max_length=10)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+
+def predict(dataset: datasets.Dataset, model: str) -> None:
+    data = datasets.Dataset.to_pandas(dataset)
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    model = T5ForConditionalGeneration.from_pretrained(model).to(device)
+
+    references = data['custom_fn_name']
+
+    predictions = [
+        predict_function_name(tokenizer, model, i).split(' ')
+        for i in data['custom_with_comments']
+    ]
+    predictions = [i[1] if len(i) > 1 else i[0] for i in predictions]
+
+    eval_results = run_evaluate(predictions, references)
+    pretty_print(
+        "Evaluation results for functions with comments:", eval_results
+    )
+
+    predictions = [
+        predict_function_name(tokenizer, model, i).split(' ')
+        for i in data['custom_without_comments']
+    ]
+    predictions = [i[1] if len(i) > 1 else i[0] for i in predictions]
+
+    eval_results = run_evaluate(predictions, references)
+    pretty_print(
+        "Evaluation results for functions without comments:", eval_results
+    )
 
 
 def run_evaluate(
